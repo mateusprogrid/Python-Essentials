5a2d65c04ab36fefddde3d57434003ce6082543f
Mateusmelo-C <mateusmelo.info@gmail.com>
2025-09-25 14:52:48 -0300

    update

--- first.py
+++ first.py
@@ -1,1 +1,5 @@
+""" 
+Python Essentials 1 (Cisco Networking Academy) e a certificação 
+PCEP — Certified Entry-Level Python Programmer (Python Institute). 
+"""
 print("Hello World!!") #Tradição da programação!
